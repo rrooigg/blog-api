7e438dadb12305899f55916871d169cd09c09a05
rrooigg <devnadifa@gmail.com>
2026-03-30 11:18:49 +0300

    defining the models

--- myblog/api/models.py
+++ myblog/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+  title = models.CharField(max_length=100)
+  content = models.TextField()
+  published_date = models.DateTimeField(auto_now_add=True) # automatically fills this field whenever a blog post is created
+
+  def __str__(self):
+    return self.title
