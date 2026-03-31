49da849fc973a0909869a241bc41e4d4a9449e10
rrooigg <devnadifa@gmail.com>
2026-03-31 08:02:38 +0300

    another method to delete(overriding)

--- myblog/api/views.py
+++ myblog/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics # shows the UI to perform CRUD operations
+from rest_framework import generics, status # shows the UI to perform CRUD operations
+from rest_framework.response import Response
 from .models import BlogPost
 from .serializers import BlogPostSerializer
 
@@ -7,6 +8,12 @@ from .serializers import BlogPostSerializer
 class BlogPostListCreate(generics.ListCreateAPIView):
   queryset = BlogPost.objects.all() # gets all posts that exists
   serializer_class = BlogPostSerializer
+
+  # another method to delete(overriding)
+  def delete(self, request, *args, **kwargs):
+    BlogPost.objects.all().delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class BlogPostRetrieveUpdateDestory(generics.RetrieveUpdateDestroyAPIView):
   queryset = BlogPost.objects.all()
