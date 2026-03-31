5949e0807eb98b93ea8090e5da7b1d79613ec6ae
rrooigg <devnadifa@gmail.com>
2026-03-31 08:20:07 +0300

    manually build endpoints

--- myblog/api/views.py
+++ myblog/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, status # shows the UI to perform CRUD opera
 from rest_framework.response import Response
 from .models import BlogPost
 from .serializers import BlogPostSerializer
+from rest_framework.views import APIView
 
 # Create your views here.
 class BlogPostListCreate(generics.ListCreateAPIView):
@@ -19,3 +20,18 @@ class BlogPostRetrieveUpdateDestory(generics.RetrieveUpdateDestroyAPIView):
   queryset = BlogPost.objects.all()
   serializer_class = BlogPostSerializer
   lookup_field = "pk" # primary key = id
+
+class BlogPostList(APIView):
+  def get(self, request, format=None):
+    # get title from the query parameters(if none, default to empty string)
+    title = request.query_params.get("title", "")
+    
+    if title:
+      # filter queryset based on title
+      blog_posts = BlogPost.objects.filter(title__icontains=title)
+    else:
+      # if no title is provided, return all blog posts
+      blog_posts = BlogPost.objects.all()
+
+    serializer = BlogPostSerializer(blog_posts, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
